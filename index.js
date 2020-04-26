dbff6efce90e6646f6abdb4df3319e376b198296
francescodist <francesco.dstf@gmail.com>
2020-04-26 15:22:54 +0200

    Add ocr example

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
+const { createWorker } = require('tesseract.js');
+
+const worker = createWorker({
+    logger: m => console.log(m)
+});
+
+(async () => {
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    // await worker.terminate();
+})();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Ok');
+app.get('/', async (req, res) => {
+    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+    console.log(text);
+    res.send(text);
 })
 
 app.listen(PORT);
