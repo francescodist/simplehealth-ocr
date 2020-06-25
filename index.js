b7fbe40ea240be5d7dc3654e0c4c51d17f465383
Lero <leroadmin@localhost.localdomain>
2020-06-25 03:42:47 -0400

    Add stuff

--- index.js
+++ index.js
@@ -2,55 +2,94 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3000;
+const fs = require('fs');
+const shell = require('shelljs');
+const formidable = require('formidable');
+const PORT = 3000;
+
+const bodyParserConfig = {
+    limit: '50mb',
+    defer: true
+}
 
 app.use(cors());
-app.use(bodyParser({
-    limit: '50mb'
-}));
-const { createWorker, createScheduler } = require('tesseract.js');
-
-const worker = createWorker({
-    logger: m => console.log("Worker 1", m)
-});
-
-const worker2 = createWorker({
-    logger: m => console.log("Worker 2", m)
-});
-
-let scheduler;
-
-(async () => {
-    await worker.load();
-    await worker.loadLanguage('ita');
-    await worker.initialize('ita');
-    await worker2.load();
-    await worker2.loadLanguage('ita');
-    await worker2.initialize('ita');
-    scheduler = createScheduler();
-    scheduler.addWorker(worker);
-    scheduler.addWorker(worker2);
-    // await worker.terminate();
-})();
+app.use(bodyParser.json(bodyParserConfig));
+app.use(bodyParser.urlencoded({...bodyParserConfig, extended: true}));
 
 app.get('/', async (req, res) => {
-    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-    console.log(text);
-    res.send(text);
+    res.send("ok");
 })
 
-app.post('/ocr', async (req, res) => {
+app.post('/:lang(ita|eng)', async (req, res) => {
+    const filename = Math.random().toString(36).substring(7);
     try {
+	const {lang} = req.params 
         const { image } = req.body;
-        console.log(image);
-        const { data: { text } } = await scheduler.addJob('recognize', image);
-        console.log(text);
-        res.send(text);
+	const data = image.replace(/^data:image\/\w+;base64,/, "");
+	const buffer =  Buffer.from(data, 'base64');
+	fs.writeFileSync(`${filename}.png`,buffer);
+	shell.exec(`./textcleaner -g -e stretch -f 25 -o 10 -u -s 1 -T -p 10 ${filename}.png ${filename}-opt.png`);
+	shell.exec(`tesseract -l ${lang} ${filename}-opt.png ${filename}`);
+	const text = fs.readFileSync(`${filename}.txt`, 'utf8');
+	res.json({text});
+	console.log(new Date().toUTCString(),"\n",text);
     } catch (e) {
-        console.log(e)
+        console.log(new Date().toUTCString(),e)
         res.send(e);
+    } finally {
+    	fs.unlink(`${filename}.png`, () => {});
+        fs.unlink(`${filename}-opt.png`, () => {});  
+        fs.unlink(`${filename}.txt`, () => {});
     }
 
 })
 
-app.listen(PORT);
+app.post("/pdf/:lang(ita|eng)", (req,res) => {
+    var form = new formidable.IncomingForm();
+    //Formidable uploads to operating systems tmp dir by default
+    form.uploadDir = ".";       //set upload directory
+    form.keepExtensions = true;     //keep file extension
+
+    form.parse(req, function(err, fields, files) {
+	if(!err) {
+		const upload = files.pdf.path;
+		const filename = Math.random().toString(36).substring(7);
+		const {stdout: pages} = shell.exec(`pdfinfo ${upload} | grep Pages | sed 's/[^0-9]*//'`);
+		shell.exec(`pdftoppm ${upload} ${filename} -png`);
+		try {
+       			const {lang} = req.params;
+			let text = "";
+			for(let i = 1; i<= pages; i++) {
+				shell.exec(`./textcleaner -g -e stretch -f 25 -o 10 -u -s 1 -T -p 10 ${filename}-${i}.png ${filename}-${i}-opt.png`);
+                        	shell.exec(`tesseract -l ${lang} ${filename}-${i}-opt.png ${filename}-${i}`);
+				text += fs.readFileSync(`${filename}-${i}.txt`, 'utf8');
+			}
+        		res.json({text});
+        		console.log(new Date().toUTCString(),"\n",text);
+    		} catch (e) {
+        		console.log(new Date().toUTCString(),e)
+        		res.send(e);
+    		} finally {
+			fs.unlink(upload, () => {});
+			for(let i = 1; i <= pages; i++) {
+				fs.unlink(`${filename}-${i}.png`, () => {});
+        	                fs.unlink(`${filename}-${i}-opt.png`, () => {});
+	                        fs.unlink(`${filename}-${i}.txt`, () => {});
+
+			}
+   		 }
+
+	} else {
+		res.send(err);
+	}
+    });
+})
+
+function ocrPic(req,res,filename) {
+
+}
+
+
+app.listen(PORT, ()=>{
+    console.log("Listening on Port " + PORT);
+});
